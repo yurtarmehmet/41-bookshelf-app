fd5d43fb00958affa103103080913baf72f37284
Mehmet Yurtar <mehmetyurtar@gmail.com>
2020-05-17 15:54:21 +0300

    Updates

---
+++ precache-manifest.15746e2c69b917f0a05604b7d40e886a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0982091ce4a3beb90ba67d5ec9f118",
+    "url": "/41-bookshelf-app/index.html"
+  },
+  {
+    "revision": "f4da606abcd6712e0efb",
+    "url": "/41-bookshelf-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5136a77ea1d3743ba029",
+    "url": "/41-bookshelf-app/static/css/main.45e4863e.chunk.css"
+  },
+  {
+    "revision": "f4da606abcd6712e0efb",
+    "url": "/41-bookshelf-app/static/js/2.7ff12515.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/41-bookshelf-app/static/js/2.7ff12515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5136a77ea1d3743ba029",
+    "url": "/41-bookshelf-app/static/js/main.f0de581d.chunk.js"
+  },
+  {
+    "revision": "abfa5284547aaaed6169",
+    "url": "/41-bookshelf-app/static/js/runtime-main.9215596e.js"
+  }
+]);
